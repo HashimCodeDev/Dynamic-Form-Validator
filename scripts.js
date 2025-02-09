192023cb5ee6b993996c510f453640d7ad74e20b
Hashim <hashimmohammed212@gmail.com>
2025-02-09 06:31:48 +0530

    js update

--- scripts.js
+++ scripts.js
@@ -5,15 +5,15 @@ const password = document.getElementById('password');
 // const confirmPassword = document.getElementById('confirmPassword');
 const number = document.getElementById('number');
 const age = document.getElementById('age');
-let errorMessage = document.getElementById("error-message");
+let errorMessage = document.getElementById('error-message');
 let isValid = true;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  checkInputs();
-
-  form.reset();
-  alert('Successfully submitted')
+  if (checkInputs()) {
+    form.reset();
+    alert('Successfully submitted');
+  }
 });
 
 function isValidEmail(email) {
@@ -31,37 +31,28 @@ function checkInputs() {
 
   if (!/^[A-Za-z]+$/.test(nameValue)) {
     errorMessage.innerText = 'Name should only contain letters';
-    isValid = false
-  }
-
-  else if (!isValidEmail(emailValue)) {
+    isValid = false;
+  } else if (!isValidEmail(emailValue)) {
     errorMessage.innerText = 'Invalid email format';
-    isValid = false
+    isValid = false;
   }
 
   // if (passwordValue != confirmPasswordValue){
   //     setError('passwordError', 'Passwords do not match');
   // }
-
   else if (!/^\d+$/.test(numberValue)) {
     errorMessage.innerText = 'Invalid email format';
-    isValid = false
-  }
-
-  else if (numberValue.length != 10) {
+    isValid = false;
+  } else if (numberValue.length != 10) {
     errorMessage.innerText = 'Invalid email format';
-    isValid = false
-  }
-
-  else if (!/^\d+$/.test(ageValue)) {
+    isValid = false;
+  } else if (!/^\d+$/.test(ageValue)) {
     errorMessage.innerText = 'Invalid email format';
-    isValid = false
-  }
-
-  else if (age > 140) {
+    isValid = false;
+  } else if (age > 140) {
     errorMessage.innerText = 'Invalid email format';
-    isValid = false
+    isValid = false;
   }
 
-  return isValid
+  return isValid;
 }
